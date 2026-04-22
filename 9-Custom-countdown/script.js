9493268023de664802e1782cf6e1e89210707546
bug2007 <moumitah2007@gmail.com>
2026-04-22 21:08:11 +0600

    form values

--- 9-Custom-countdown/script.js
+++ 9-Custom-countdown/script.js
@@ -2,7 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // set date input min with today's date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
+// take values from form input
+function updateCountdown(e) {
+    e.preventDefault(); // prevents from refreshing the page on submitting the form
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+// event listeners
+countdownForm.addEventListener('submit', updateCountdown);
