938b292c0aae1375fb8d1770c29963aae1ec9db6
bug2007 <moumitah2007@gmail.com>
2026-04-28 20:04:07 +0600

    countdown page js

--- 15-Math-sprint-game/script.js
+++ 15-Math-sprint-game/script.js
@@ -83,6 +83,27 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+// Displays 3, 2, 1, GO!
+function countdownStart() {
+  countdown.textContent = '3';
+  setTimeout(() => {
+    countdown.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = 'GO!';
+  }, 3000);
+}
+
+// Navigate from splash page to countdown page
+function showCountdown() {
+  countdownPage.hidden = false;
+  splashPage.hidden = true; 
+  countdownStart();
+}
+
 // Get the value from selected radio button
 function getRadioValue() {
   let radioValue;
@@ -97,6 +118,9 @@ function getRadioValue() {
 function selectQuestionAmount(e) {
   e.preventDefault(); 
   questionAmount = getRadioValue();
+  if (questionAmount) {
+    showCountdown();
+  }
 }
 
 // Event listeners
