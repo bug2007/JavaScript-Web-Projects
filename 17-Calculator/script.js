f828b1800e2156919319667166db8e1217ed02a3
bug2007 <moumitah2007@gmail.com>
2026-04-30 12:28:11 +0600

    calculation logic js

--- 17-Calculator/script.js
+++ 17-Calculator/script.js
@@ -27,13 +27,31 @@ function addDecimal() {
     }
 }
 
+// Calculate first and second values depending on operator
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber
+}
+
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    }    
     // Assign firstValue if no value
     if (!firstValue) {
         firstValue = currentValue;
     } else {
-
+        // console.log(firstValue, operatorValue, currentValue);
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        // console.log('calculation', calculation);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
     }
     // Ready for next value, store operator
     awaitingNextValue = true;
